f70e5425b85c4d650fa682d5d58ccc3c788bc038
Cse15-0737 <aryanbrijrajrai@gmail.com>
2026-08-19 11:21:52 +0530

    API and FIle System classwork

---
+++ API.js
@@ -0,0 +1,10 @@
+const os = require("os");
+console.log("Free Memory: ", os.freemem());
+console.log("OS version: ", os.version());
+console.log("OS release: ", os.release());
+console.log("OS platform: ", os.platform());
+console.log("OS runtime: ", os.uptime());
+console.log("Platform: ", os.platform());
+console.log("Architecture: ", os.arch());
+console.log("CPU info: ", os.cpus());
+console.log("Hostname: ", os.hostname());
