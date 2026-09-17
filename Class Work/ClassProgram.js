74dcbe0a811cddd6e492880ae2c5cfa0fa1ac30b
Cse15-0737 <aryanbrijrajrai@gmail.com>
2026-09-17 13:59:15 +0530

    Class Work "17/09/26"

--- Class Work/ClassProgram.js
+++ Class Work/ClassProgram.js
@@ -1,6 +1,37 @@
 // Using Node.js's built in events module, create an EventEmitter.  Register Multiple listeners for a response
 // event, then emit the event by passing name and id as arguments and display them in the console.
-import http from "http";
-import EventEmitter from "events";
-
+const http = require("http");
+const EventEmitter = require("events");
+ 
 const myEmitter = new EventEmitter();
+
+myEmitter.on("response", () => {
+    console.log("data received");
+});
+
+myEmitter.on("response", () => {
+    console.log("some other logic here");
+});
+
+myEmitter.on("response", (name, id) => {
+    console.log(`Name: ${name}, ID: ${id}`);
+});
+
+myEmitter.emit("response", "aaryan", 2);
+
+// Using Node.js's built in events module, create an HTTP server that runs on port 3000 and returns a
+// JSON response for every request containing id: 1 and name: "Laptop"
+
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "application/json"
+
+   });
+  res.end(JSON.stringify({ id: 1, 
+    name: "Laptop" }));
+});
+
+server.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
